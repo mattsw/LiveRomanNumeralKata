af3fbed569d8f6b6730a7c6804019808925ec85d
Matt Wilhelm <mattw@stcu.org>
2015-11-03 11:42:15 -0800

    Initial commit

---
+++ LiveDevKata.Tests/RomanConverterTests.cs
@@ -0,0 +1,32 @@
+﻿namespace LiveDevKata.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RomanConverterTests
+    {
+        [TestFixture]
+        public class ConvertToRoman
+        {
+            private Func<int, string> methodToTest;
+
+            [TestFixtureSetUp]
+            public void Setup()
+            {
+                methodToTest = new RomanConverter().ConvertToRoman;
+            }
+            
+            [TestCase(1, "I")]
+            [TestCase(2, "II")]
+            [TestCase(3, "III")]
+            [TestCase(4, "IV")]
+            [TestCase(5, "V")]
+            [TestCase(7, "VII")]
+            public void IntegerInput_ShouldReturnRomanString(int input, string expected)
+            {
+                Assert.AreEqual(expected, methodToTest(input));
+            }
+        }
+    }
+}
---
+++ LiveDevKata/LiveDevKata/RomanConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace LiveDevKata
+{
+    using System.Collections.Generic;
+
+    public class RomanConverter
+    {
+        public string ConvertToRoman(int input)
+        {
+            string result = string.Empty;
+
+            new List<KeyValuePair<int, string>>
+            {
+                new KeyValuePair<int, string>(5, "V"),
+                new KeyValuePair<int, string>(4, "IV"),
+                new KeyValuePair<int, string>(1, "I")
+            }.ForEach(pair =>
+            {
+                while (input >= pair.Key)
+                {
+                    input -= pair.Key;
+                    result += pair.Value;
+                }
+            });
+
+
+            return result;
+        }
+    }
+}
